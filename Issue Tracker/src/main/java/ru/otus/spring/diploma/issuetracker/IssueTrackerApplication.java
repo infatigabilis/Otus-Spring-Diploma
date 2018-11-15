8f0c9f42df8068cc2f3ba46624428130785f6a67
infatigabilis <infatigabilis@gmail.com>
2018-11-15 21:48:30 +1000

    Service Registry: init

--- Issue Tracker/src/main/java/ru/otus/spring/diploma/issuetracker/IssueTrackerApplication.java
+++ Issue Tracker/src/main/java/ru/otus/spring/diploma/issuetracker/IssueTrackerApplication.java
@@ -2,10 +2,12 @@ package ru.otus.spring.diploma.issuetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
 @EnableReactiveMongoRepositories
+@EnableEurekaClient
 public class IssueTrackerApplication {
 
     public static void main(String[] args) {
