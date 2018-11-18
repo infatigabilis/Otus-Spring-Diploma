fa1ce5d6948f4b8078edffb531b4c2cd733e1539
infatigabilis <infatigabilis@gmail.com>
2018-11-19 04:25:33 +1000

    UI Client: complete IssueList static

--- UI Client/src/App.js
+++ UI Client/src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import CssBaseline from "@material-ui/core/CssBaseline/CssBaseline";
+import Grid from "@material-ui/core/Grid/Grid";
+import AppBar from "@material-ui/core/AppBar/AppBar";
+import Toolbar from "@material-ui/core/Toolbar/Toolbar";
+import Typography from "@material-ui/core/Typography/Typography";
+import IssueList from "./components/IssueList";
 
-class App extends Component {
+const styles = {
+  root: {
+    flexGrow: 1,
+    padding: 24
+  }
+};
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+
+        <Grid container spacing={24} style={styles.root}>
+
+          <Grid item xs={5} style={styles.grid}>
+            <IssueList/>
+          </Grid>
+
+          <Grid item xs={7} style={styles.grid}>
+            <AppBar position="static" color="primary">
+              <Toolbar variant="dense">
+                <Typography variant="h6" color="inherit">
+                  OTUS-12
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Grid>
+
+        </Grid>
+
+      </React.Fragment>
     );
   }
 }
-
-export default App;
---
+++ UI Client/src/components/IssueList.js
@@ -0,0 +1,131 @@
+import React, {Component} from 'react';
+import Grid from "@material-ui/core/Grid/Grid";
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import AppBar from "@material-ui/core/AppBar/AppBar";
+import Toolbar from "@material-ui/core/Toolbar/Toolbar";
+import Typography from "@material-ui/core/Typography/Typography";
+import List from "@material-ui/core/List/List";
+import ListItem from "@material-ui/core/ListItem/ListItem";
+import green from "@material-ui/core/colors/green";
+import Chip from "@material-ui/core/Chip/Chip";
+import Icon from "@material-ui/core/Icon/Icon";
+import IconButton from "@material-ui/core/IconButton/IconButton";
+
+const theme = createMuiTheme();
+
+const styles = {
+  list: {
+    backgroundColor: theme.palette.background.paper
+  },
+  listItem: {
+    paddingBottom: 4
+  },
+  chip: {
+    marginLeft: theme.spacing.unit,
+    fontSize: 11,
+    height: 23,
+  },
+  priorityIcon: {
+    fontSize: 40,
+    margin: '-7px 0 -6px 0'
+  },
+  highPriorityIcon: {
+    fontSize: 30,
+    margin: '-2px 0 -1px 0'
+  },
+  issueTitle: {
+    marginLeft: 10
+  },
+  issueStateGrid: {
+    textAlign: 'center'
+  },
+  orderByLabel: {
+    textAlign: 'right'
+  },
+  orderByIcon: {
+    color: '#fff',
+    fontSize: 30
+  },
+  orderByButton: {
+    height: 30,
+    width: 30
+  }
+};
+
+export default class IssueList extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <AppBar position="static" color="primary">
+          <Toolbar variant="dense">
+            <Grid container alignItems={'center'}>
+              <Grid item xs={9}>
+                <Typography variant="h6" color="inherit">Your issues</Typography>
+              </Grid>
+              <Grid item xs={1} style={styles.issueStateGrid}>
+                <IconButton style={styles.orderByButton} component="span">
+                  <Icon style={styles.orderByIcon} color="error">arrow_drop_up</Icon>
+                </IconButton>
+              </Grid>
+              <Grid item xs={2} style={styles.issueStateGrid}>
+                <IconButton style={styles.orderByButton} component="span">
+                  <Icon style={styles.orderByIcon}>arrow_drop_down</Icon>
+                </IconButton>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+        <List component="nav" style={styles.list}>
+          <ListItem button style={styles.listItem}>
+            <Grid container>
+              <Grid item xs={2}>
+                <Typography variant="subtitle1"><b>OTUS-1</b></Typography>
+              </Grid>
+              <Grid item xs={7}>
+                <Typography variant="subtitle1" style={styles.issueTitle}>Spring Data MongoDB</Typography>
+              </Grid>
+              <Grid item xs={1} style={styles.issueStateGrid}>
+                <Icon style={styles.priorityIcon} color="error">expand_less</Icon>
+              </Grid>
+              <Grid item xs={2} style={styles.issueStateGrid}>
+                <Chip label="DEVELOPMENT" style={styles.chip} variant="outlined" clickable={true} />
+              </Grid>
+            </Grid>
+          </ListItem>
+          <ListItem button selected={true} style={styles.listItem}>
+            <Grid container>
+              <Grid item xs={2}>
+                <Typography variant="subtitle1"><b>OTUS-12</b></Typography>
+              </Grid>
+              <Grid item xs={7}>
+                <Typography variant="subtitle1" style={styles.issueTitle}>Spring Cloud: Config Server, Service Registry</Typography>
+              </Grid>
+              <Grid item xs={1} style={styles.issueStateGrid}>
+                <Icon style={styles.highPriorityIcon} color="error">arrow_upward</Icon>
+              </Grid>
+              <Grid item xs={2} style={styles.issueStateGrid}>
+                <Chip label="REVIEW" style={styles.chip} variant="outlined" clickable={true} />
+              </Grid>
+            </Grid>
+          </ListItem>
+          <ListItem button style={styles.listItem}>
+            <Grid container>
+              <Grid item xs={2}>
+                <Typography variant="subtitle1"><b>SPRING-9</b></Typography>
+              </Grid>
+              <Grid item xs={7}>
+                <Typography variant="subtitle1" style={styles.issueTitle}>Spring Batch</Typography>
+              </Grid>
+              <Grid item xs={1} style={styles.issueStateGrid}>
+                <Icon style={Object.assign({}, styles.priorityIcon, {color: green["500"]})} >expand_more</Icon>
+              </Grid>
+              <Grid item xs={2} style={styles.issueStateGrid}>
+                <Chip label="NEW" style={styles.chip} variant="outlined" clickable={true} />
+              </Grid>
+            </Grid>
+          </ListItem>
+        </List>
+      </React.Fragment>
+    );
+  }
+}
